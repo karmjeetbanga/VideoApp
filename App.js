867859926191d47bf3e83013d30de43b91b2b9c0
Karmjeet Kaur <karmjeet.k@adorebeauty.com.au>
2022-09-29 22:35:25 +1000

    adding basic functionality

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
+import react from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
+import SignInScreen from './Screens/SignInScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SignInScreen/>
   );
 }
 

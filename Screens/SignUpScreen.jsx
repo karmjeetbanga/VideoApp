ffe3cab3eab07cbb8e836d81946c7bcaa55d1101
Karmjeet Kaur <karmjeet.k@adorebeauty.com.au>
2022-09-29 22:36:16 +1000

    adding screen folder

---
+++ Screens/SignUpScreen.jsx
@@ -0,0 +1,19 @@
+import react from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function SignUpScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Sign In</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
